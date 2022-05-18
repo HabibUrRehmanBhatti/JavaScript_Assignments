8cdf44bc8ded951ba7eca618894db0b9ce55e832
Habib Ur Rehman Bhatti <101096941+HabibUrRehmanBhatti@users.noreply.github.com>
2022-05-18 16:57:55 +0500

    Add files via upload

---
+++ chapter#5/app.js
@@ -0,0 +1,57 @@
+// ================>Addition<===================
+let a = 2
+let b = 4
+let result=( a + b)
+document.write(" sum of 2 and 4 is " ,result+"<br>")
+
+// ================>Subtration<===================
+let e = 2
+let f = 4
+let result2=( e - f)
+document.write(" Subtration of 2 and 4 is " ,result2 +"<br>")
+// ================>multiplication<===================
+let g = 2
+let h = 4
+let result3=( g * h)
+document.write(" multiplication of 2 and 4 is " ,result3 +"<br>")
+// ================>division<===================
+let x = 2
+let y = 4
+let result4=( x / y)
+document.write(" division of 2 and 4 is " ,result4 +"<br>"+"<br>"+"<br>"+"<br>")
+// // ================>Modulus<===================
+// let c = 2
+// let d = 4
+// let result5=( c + d)
+// document.write("sum of 2 and 4 is " ,result )
+
+
+
+//       ======> TASK # 3 <======
+
+
+
+                // ======> TASK # 4 <======
+
+    
+let watch = 500
+let people = 5
+let totalCost = (watch*people)
+document.write("The Total Cost for buying watches for 5 people is " ,totalCost )
+
+
+                // ======> TASK # 5 <======
+
+
+                var i, j;
+                 for (i = 5; i <= 5; i++) {
+                 document.write("Table" + " " + i + "<br>");
+                 for (j = 1; j <= 10; j++)
+                 document.write(i + "*" + j + "=" + i * j + "<br>");
+                 + "<br>"+ "<br>"
+                 }
+                // ======> TASK # 6 <======
+let totalMarks = 1000
+let obtainedMarks = 670
+let percentage = obtainedMarks/totalMarks*100 +"%"
+document.write(percentage)
