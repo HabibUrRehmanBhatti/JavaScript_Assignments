7a2671413abbfc822e926e68adc038f9347742d7
Habib Ur Rehman Bhatti <101096941+HabibUrRehmanBhatti@users.noreply.github.com>
2022-05-18 14:32:19 +0500

    Add files via upload

---
+++ chapter#3/app.js
@@ -0,0 +1,22 @@
+                                                                        // Chapter # 3
+ 
+ // Question # 1
+let age = 19
+alert("Iam "+age+" year old" )
+
+
+
+
+ 
+// Question # 2
+ let visit= 13
+alert("you have visited this site "+visit+" times" )
+
+
+  
+// Question # 3
+ let birthYear= 2002
+document.write("My birth yea is " + birthYear+ "<br>")
+
+document.write(typeof birthYear)
+
