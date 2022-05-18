cc43cde3a626e75902b44ecf55aea6133f6ca4de
Habib Ur Rehman Bhatti <101096941+HabibUrRehmanBhatti@users.noreply.github.com>
2022-05-18 14:53:29 +0500

    Add files via upload

---
+++ chapter#4/app.js
@@ -0,0 +1,33 @@
+// ==========>  CHAPTER # 4  <==========
+
+            
+  //       ======> TASK # 1 <======
+
+let lanGname = "javasScript"
+let releaseDate = "4thdecember,1995"
+let createdBy = "Brendan Eich"
+alert(name +" "+releaseDate +" "+createdBy)
+
+                // ======> TASK # 2 <======
+ var name = ("legal variable");
+var ali1;
+var $mobile;
+var nokia12;
+var x1;
+var b01;  
+ var name = ("Illegal variable");
+ var 1ali;
+ var ?mobile;
+ var %a ;
+ var *blur;
+
+
+
+            //    ======> TASK # 3 <======
+    
+document.write("<h1> “Rules for naming JS variables” </h1>")
+
+document.write("<h4>Variable names can only contain ______, ______, ______ and ______. For example $my_1stVariable </h4>");
+document.write("<h4>Variables must begin with a ______, ______ or _____. For example $name, _name or nam</h4>")
+document.write("<h4> Variable names are case _________ </h4>")
+document.write("<h4>Variable names should not be JS _______</h4>")
