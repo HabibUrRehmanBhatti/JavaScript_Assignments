5afbf2c66e68159f01b3db07793f985e7ff28a86
Habib Ur Rehman Bhatti <101096941+HabibUrRehmanBhatti@users.noreply.github.com>
2022-06-09 16:40:38 +0500

    Add files via upload

---
+++ chap12-13/app.js
@@ -0,0 +1,81 @@
+// // -------- Chapter # 12-13 ----------- 
+
+// // Task 1
+// var x = +prompt("Enter any num from 65 to 90 or 97 t0 192");
+// if (x >= 65 && x <= 90){
+//     alert("The letter is capital.");
+// } else {
+//     if (x >= 97 && x <= 122){
+//         alert("The letter is lowercase.");
+//     }
+// }
+// Task 2
+// var firstnum= +prompt("Enter first num")
+// var secondnum= +prompt("Enter second num")
+// if (firstnum>secondnum){
+//     alert("firstnum is greater")
+// }
+// else{
+// if(firstnum<secondnum){
+//     alert("secondnum is greater")
+// }
+// } if(firstnum===secondnum){
+//     alert("both are equal")
+// }
+// Task 3
+// var x = +prompt("Enter any num:");
+// if (x < 0) {
+//     alert("The number is negative.");
+// }else {
+//     if (x > 0){
+//         alert("The number is positive.");
+//     }else {
+//         if (x === 0){
+//         alert("The number is zero.");
+//         }
+//     }
+// }
+//task 4
+// let input = prompt("enter any letter")
+// if (input ==="a"|| input ==="e"|| input ==="i"|| input ==="o"|| input ==="u"){
+//     alert("this letter is vowel")
+// }else{
+//     alert("this letter is not a vowel")
+// }
+//task 5
+// let correctpassword = "unibro"
+// let ps1 = prompt ("enter your password")
+// let ps2 = prompt ("enter your password again")
+// if (ps1 === correctpassword){
+// }if(ps2===correctpassword){
+//     alert (" password is correct")
+
+// }else{alert("pasword is wrong")}
+// Task 6
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) 
+// { greeting = "Good day"; 
+// }else {
+//     greeting = "Good evening";
+//      }
+
+// Task 7
+// var time = +prompt("Enter time in 24 hours format:");
+// if (time >= 0000 && time < 1200){
+//     alert("Good Morning!");
+// }else {
+//     if (time >= 1200 && time < 1700){
+//         alert("Good Afernoon!");
+//     }else {
+//         if(time >= 1700 && time < 2100){
+//             alert("Good Evening!");
+//         }else {
+//             if(time >= 2100 && time <= 2359){
+//                 alert("Good Night!");
+//             }
+//         }
+//     }
+// }                                      
+
+
